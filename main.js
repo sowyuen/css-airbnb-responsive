cf8ad12e3f731a1a83ba8e327c71911032591d53
sowyuen <sowyuenl@gmail.com>
2019-07-03 10:54:24 +0800

    morning exercise

---
+++ main.js
@@ -0,0 +1,21 @@
+var guideBooks = document.querySelector("#guidebooks");
+guideBooks.addEventListener("click",function(){alert("This feature is not done yet!")});
+
+var destinations = document.querySelector("#destinations");
+destinations.addEventListener("click",function(){alert("This feature is not done yet!")});
+
+var sansfrans = document.querySelector("#SF");
+sansfrans.addEventListener("click",function(){window.open("https://www.google.com.sg/maps/place/Sonoma,+CA+95476,+USA/@38.291092,-122.4666091,14z/data=!3m1!4b1!4m5!3m4!1s0x8085ac43817b9d77:0x380ed210ee6e045!8m2!3d38.291859!4d-122.4580356?hl=en")});
+
+var newyork = document.querySelector("#NY");
+newyork.addEventListener("click",function(){window.open("https://www.google.com.sg/maps/place/Sonoma,+CA+95476,+USA/@38.291092,-122.4666091,14z/data=!3m1!4b1!4m5!3m4!1s0x8085ac43817b9d77:0x380ed210ee6e045!8m2!3d38.291859!4d-122.4580356?hl=en")});
+
+var london = document.querySelector("#London");
+london.addEventListener("click",function(){window.open("https://www.google.com.sg/maps/place/Sonoma,+CA+95476,+USA/@38.291092,-122.4666091,14z/data=!3m1!4b1!4m5!3m4!1s0x8085ac43817b9d77:0x380ed210ee6e045!8m2!3d38.291859!4d-122.4580356?hl=en")});
+
+var napa = document.querySelector("#Napa");
+napa.addEventListener("click",function(){window.open("https://www.google.com.sg/maps/place/Sonoma,+CA+95476,+USA/@38.291092,-122.4666091,14z/data=!3m1!4b1!4m5!3m4!1s0x8085ac43817b9d77:0x380ed210ee6e045!8m2!3d38.291859!4d-122.4580356?hl=en")});
+var sonoma = document.querySelector("#Sonoma");
+sansfrans.addEventListener("click",function(){window.open("https://www.google.com.sg/maps/place/Sonoma,+CA+95476,+USA/@38.291092,-122.4666091,14z/data=!3m1!4b1!4m5!3m4!1s0x8085ac43817b9d77:0x380ed210ee6e045!8m2!3d38.291859!4d-122.4580356?hl=en")});
+var sansfranss = document.querySelector("#SF2");
+sansfranss.addEventListener("click",function(){window.open("https://www.google.com.sg/maps/place/Sonoma,+CA+95476,+USA/@38.291092,-122.4666091,14z/data=!3m1!4b1!4m5!3m4!1s0x8085ac43817b9d77:0x380ed210ee6e045!8m2!3d38.291859!4d-122.4580356?hl=en")});
